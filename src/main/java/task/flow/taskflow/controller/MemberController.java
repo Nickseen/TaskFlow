27c8c882eca6f70e9da79e257af0c739e55a410c
Nicolashaaa <npetcov@mail.ru>
2025-04-25 02:23:42 +0300

    Created Member Controller

---
+++ src/main/java/task/flow/taskflow/controller/MemberController.java
@@ -0,0 +1,59 @@
+package task.flow.taskflow.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import task.flow.taskflow.model.Member;
+import task.flow.taskflow.model.ProjectRole;
+import task.flow.taskflow.service.MemberService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/members")
+public class MemberController {
+    private final MemberService service;
+
+    @Autowired
+    public MemberController(MemberService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/project/{project_name}")
+    public List<Member> findByProjectName(@PathVariable("project_name") String project_name) {
+        return service.findMembersByProjectName(project_name);
+    }
+
+    @GetMapping("/project/{project_name}/role/{role}")
+    public List<Member> findMembersByProjectNameAndRole(
+            @PathVariable("project_name") String project_name,
+            @PathVariable("role") ProjectRole role) {
+        return service.findMembersByProjectNameAndRole(project_name, role);
+    }
+
+    @GetMapping("/project/{project_name}/participant/{email}")
+    public Member findMemberByProjectAndParticipant(
+            @PathVariable("project_name") String project_name,
+            @PathVariable("email") String email) {
+        return service.findMemberByProjectAndParticipant(project_name, email);
+    }
+
+    @PostMapping("/add_member")
+    public Member saveMember(@RequestBody Member member) {
+        return service.saveMember(member);
+    }
+
+    @PutMapping("/update_role")
+    public Member updateMemberRole(@RequestBody Member member) {
+        return service.saveMember(member);
+    }
+
+    @DeleteMapping("/remove/{project_name}/{email}")
+    public void removeMember(
+            @PathVariable("project_name") String project_name,
+            @PathVariable("email") String email) {
+        service.removeMember(project_name, email);
+    }
+
+
+}
